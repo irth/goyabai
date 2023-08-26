d988e3b10d28b64b8b2cad92bb4f3fe17538f6b2
Wojciech Kwolek <wojciech@kwolek.io>
2023-08-26 15:34:06 +0200

    look up the space index by UUID before deleting

--- spaces.go
+++ spaces.go
@@ -18,8 +18,8 @@ type Space struct {
 	IsNativeFullscreen bool   `json:"is-native-fullscreen"`
 }
 
-func (y *Yabai) Spaces() ([]Space, error) {
-	var spaces []Space
+func (y *Yabai) Spaces() ([]*Space, error) {
+	var spaces []*Space
 	err := y.query(&spaces, "spaces")
 	if err != nil {
 		return nil, err
@@ -39,7 +39,27 @@ func (y *Yabai) Space(id int) (Space, error) {
 	return space, err
 }
 
+var ErrSpaceNotFound = fmt.Errorf("space not found")
+
+func (y *Yabai) SpaceByUUID(uuid string) (*Space, error) {
+	spaces, err := y.Spaces()
+	if err != nil {
+		return nil, err
+	}
+	for _, space := range spaces {
+		if space.UUID == uuid {
+			return space, nil
+		}
+	}
+	return nil, ErrSpaceNotFound
+}
+
 func (s *Space) Destroy() error {
-	_, err := s.y.call("-m", "space", "--destroy", fmt.Sprint(s.Index))
+	// we need to obtain the space again, because the index might have changed since we obtained it
+	updatedSpace, err := s.y.SpaceByUUID(s.UUID)
+	if err != nil {
+		return err
+	}
+	_, err = s.y.call("-m", "space", "--destroy", fmt.Sprint(updatedSpace.Index))
 	return err
 }
